2df513c68b5085c37249a886374bfb58d749c08d
Sebastian <jssanabria918@misena.edu.co>
2021-08-13 18:54:24 +0200

    Método: seleccionarId($sId),
    Se corrigieron errores de la tabla orden

--- modelos/modeloOrden/OrdenDAO.php
+++ modelos/modeloOrden/OrdenDAO.php
@@ -32,44 +32,33 @@
             
         }
  /* ------------------------------------------------------------------------------------------------------------------- */
-        public function seleccionarId($ordId) {
+        public function seleccionarId($sId) {
 
-            
-            /* $planConsulta = "select  O.ordId,O.ordvalorTotal,Mn.menId,Mn.menObservacion,Ms.mesNumeroMesa,pl.plaDescripcion,
-            Tp.tipPlaAdicional,Tp.tipPlaBebida,Tp.tipPlaPostre
-            from orden O
-            join menu Mn on O.ordIdMenu= menId 
-            join plato Pl on O.ordIdMenu=plaId
-            join tipo_plato Tp on ordIdMenu=tipPlaId
-            join mesa Ms on O.ordIdMesa=Ms.mesId
-            where O.ordId = 2;"; */
+            $planConsulta = " SELECT * FROM orden O ";
+            $planConsulta .= " WHERE O.ordId =  ? ;";
 
-            $Consulta = "select * from orden O
-            where O.ordId = ?;";
+            $listar = $this->conexion->prepare($planConsulta);
 
-            $listar=$this->conexion->prepare($Consulta);
-            $listar->execute(array($ordId[0]));
+            $listar->execute(array($sId[0]));
 
             $registroEncontrado = array();
 
-            while($registro=$listar -> fetch(PDO::FETCH_OBJ)) {
+            while ($registro = $listar->fetch(PDO::FETCH_OBJ)) {
+
                 $registroEncontrado[] = $registro;
             }
 
             $this->cierreBd();
 
-    
-
-            if(!empty($registroEncontrado)){
-
-                return ["exito Seleccion Id" => true, "Registro encontrado" => $registroEncontrado];
-
-            }else {
-                return ["exito Seleccion Id" => false, "Registro encontrado" => $registroEncontrado];
+            if (!empty($registroEncontrado)) {
+                return ['exitoSeleccionId' => true, 'registroEncontrado' => $registroEncontrado];
+            } else {
+                return ['exitoSeleccionId' => false, 'registroEncontrado' => $registroEncontrado];
             }
-
-
         }
+
+
+        
 
          public function insertar($registro) {
 
@@ -81,7 +70,7 @@
 
         }
 
-         /* public function actualizar($ordDescripcion) {
+          public function actualizar($ordDescripcion) {
 
         }
         public function eliminar($ordId = array()) {
@@ -92,7 +81,7 @@
         }
         public function eliminadorLogico($ordId = array()) {
 
-        }  */
+        }  
 
      } 
      
