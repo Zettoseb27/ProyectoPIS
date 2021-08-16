d0a29d1a806e8ab4ecd087a72ae42406be833fd0
Sebastian <jssanabria918@misena.edu.co>
2021-08-16 21:23:29 +0200

    Tabla Menú
    Todos los métodos integrados de forma correcta

--- Base de datos/Datos/menu.sql
+++ Base de datos/Datos/menu.sql
@@ -6,3 +6,15 @@ insert into menu values (1,1,'Comida muy caliente',1,null,'2021-08-12 9:22:00','
 (5,4,'Sin frigoles',1,null,'2021-08-12 9:25:00','2021-08-12 9:24:00');
 
 insert into menu values (6,5,'Comida muy caliente',1,null,'2021-08-12 10:22:00','2021-08-12 11:23:00');
+/* -------------------------------------------------------------------- */
+select menId,menObservacion,menEstado,menCreated_at
+from menu ;
+/* --------------------------------------------------------------------- */
+select menId,menObservacion,menEstado,menCreated_at
+from menu 
+where menId = 1 ;
+
+/* ----------------------------------------------------------------------- */
+delete from menu Mn where Mn.menId = 1;
+
+/*-------------------------------------------------------------------------- */ 
