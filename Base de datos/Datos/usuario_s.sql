3f2d268525f114021e06ad7886c3389bb4c877e5
Santiago Arevalo Beltran <santiagoarevalobeltran@gmail.com>
2021-08-16 22:07:11 -0500

    TablaRol
    Completas

--- Base de datos/Datos/usuario_s.sql
+++ Base de datos/Datos/usuario_s.sql
@@ -1,5 +1,5 @@
-
-SELECT * FROM proyecto.usuario_s;
+use proyecto;
+SELECT * FROM usuario_s;
 
 insert into usuario_s values (1,'zettoseb27@gmail.com','123456','null',1,'','2018-05-29 11:48:38', '2018-06-08 15:18:53'),
 (2,'fren124@gmail.com','f123456','null',1,'','2018-05-29 11:48:38', '2018-06-08 15:18:53'),
