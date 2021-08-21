00a3da94c7e0a6b51200389945806835f30fa0d4
Sebastian <jssanabria918@misena.edu.co>
2021-08-21 13:34:56 -0500

    Data Table Cocina
    Funciona correctamente

--- Base de datos/Datos/Cocina.sql
+++ Base de datos/Datos/Cocina.sql
@@ -1,4 +1,6 @@
+use proyecto;
 select * from cocina;
+
 
 /* ------------------------------------------------------- */
 
@@ -8,3 +10,13 @@ from cocina Co
 join menu Mn on Co.cociIdOrden= menId
 join tipo_plato Tp on Co.cociIdOrden = tipPlaId
 join plato Pl on Co.cociIdOrden = plaId;
+
+/* ----------------------------------------------------------------------*/
+select Co.cociId, 
+Me.mesNumeroMesa,
+Tp.tipPlaPlato,Tp.tipPlaAdicional,Tp.tipPlaPostre,
+Mn.menObservacion
+from cocina Co
+join mesa Me on cociId = mesId
+join tipo_plato Tp on cociId = tipPlaId
+join menu Mn on ordId = menId;
