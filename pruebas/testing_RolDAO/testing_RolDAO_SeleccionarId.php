406ab9cb15a6c5e14e8866c1d29b5083df8e8820
Sebastian <jssanabria918@misena.edu.co>
2021-08-26 15:40:36 -0500

    phpUnit

--- pruebas/testing_RolDAO/testing_RolDAO_SeleccionarId.php
+++ pruebas/testing_RolDAO/testing_RolDAO_SeleccionarId.php
@@ -7,15 +7,9 @@ include_once PATH.'modelos/modeloRol/RolDAO.php';
 
 $rolId = array(2);
 
-<<<<<<< HEAD:pruebas/testing_RolDAO/testing_RolDAO_SeleccionarRol.php
-
-$lRol=new LibroDAO(rolid, rolnombre, rolDescripcion, rolEstado,rolUsuSesion,rol_created_at,rol_update_at);
-
-$SeleccionarRol=$Rol->seleccionarId($sId);
-=======
 $Rol = new RolDAO(SERVIDOR, BASE, USUARIO_BD, CONTRASEÑA_BD);
 $SeleccionarRol = $Rol->seleccionarId($rolId);
->>>>>>> develop:pruebas/testing_RolDAO/testing_RolDAO_SeleccionarId.php
+
 
 echo "<pre>";
 print_r($SeleccionarRol);
