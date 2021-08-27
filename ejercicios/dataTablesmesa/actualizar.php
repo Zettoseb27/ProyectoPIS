65d6b2524e82b3a9085e5ad752e69236e7bc8227
Santiago Arevalo Beltran <santiagoarevalobeltran@gmail.com>
2021-08-27 15:50:25 -0500

    gitflow-feature-stash: tablaNumeroMesa
    Terminado tabla mesa

--- ejercicios/dataTablesmesa/actualizar.php
+++ ejercicios/dataTablesmesa/actualizar.php
@@ -9,13 +9,12 @@ include 'config.php';
 if (isset($_POST['Submit'])) {
 
     $id = $_POST['id'];
-    $Id = $_POST['Id'];
-    $Plato = $_POST['Plato'];
-    $Descripcion = $_POST['Descripcion'];
-    $Precio = $_POST['Precio'];
-    $Estado = $_POST['Estado'];
+    $mesId = $_POST['mesId'];
+    $mesCantidadComenzales = $_POST['CantidadComenzales'];
+    $mesNumeroMesa = $_POST['NumeroMesa'];
+    $mesEstado = $_POST['Estado'];
     
-    $consulta="update plato set plaPrecio = '$Precio', plaEstado = '$Estado' where plaId = '$id'";
+    $consulta="update mesa set mesId ='$mesId', CantidadComenzales = '$mesCantidadComenzales' Estado = '$mesEstado'";
     
     $result=mysqli_query($connect, $consulta);
     
@@ -25,24 +24,24 @@ if (isset($_POST['Submit'])) {
 
 $id = $_GET['id'];
 
-$query = "select pl.plaId, tpl.tipPlaPlato, pl.plaDescripcion ,pl.plaPrecio, pl.plaEstado
-from plato pl
-join  tipo_plato tpl on pl.plaId = tpl.tipPlaId
+$query = "select mesId, mesCantidadComenzales, mesNumeroMesa,mesEstado 
+from mesa pl
+join  mesId, mesCantidadComenzales, mesNumeroMesa,mesEstado 
 where plaId = $id;";
 $result = mysqli_query($connect, $query);
 
 while ($row = mysqli_fetch_array($result)) {
 
-    $Id = $row['plaId'];
-    $Plato = $row['tipPlaPlato'];
-    $Descripcion = $row['plaDescripcion'];
-    $Precio = $row['plaPrecio'];
-    $Estado = $row['plaEstado'];
+    $mesId = $row['mesId'];
+    $mesCantidadComenzales = $row['CantidadComenzales'];
+    $mesNumeroMesa = $row['NumeroMesa'];
+    $mesEstado = $row['Estado'];
+    
 }
 ?>
 <html>
     <head>
-        <title>Actualizando Libro...</title>
+        <title>Actualizando Mesa...</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
@@ -52,32 +51,28 @@ while ($row = mysqli_fetch_array($result)) {
     <body>
         <div class="container" style="width: 800px; margin-top: 100px;">
             <div class="row">
-                <h3>Actualizando Libro...</h3>
+                <h3>Actualizando Mesa...</h3>
                 <div class="col-sm-6"> 
                     <form action="" method="post" name="form1">
                         <div class="form-group">
                             <input type="hidden" name="id" class="form-control" value="<?php echo $id; ?>">
                         </div>
                         <div class="form-group">
-                            <label>Id</label>
+                            <label>mesId</label>
                             <input type="text" name="Id" class="form-control" value="<?php echo $Id; ?>" readonly="readonly">
 
                         </div>
                         <div class="form-group">
-                            <label>Plato</label>
+                            <label>CantidadComenzales</label>
                             <input type="text" name="Plato" class="form-control" value="<?php echo $Plato; ?>">
                         </div>
                         <div class="form-group">
-                            <label>Descripcion</label>
+                            <label>NumeroMesa</label>
                             <input type="text" name="Descripcion" class="form-control" value="<?php echo $Descripcion; ?>">
                         </div>
                         <div class="form-group">
-                            <label>Precio</label>
-                            <input type="text" name="Precio" class="form-control" value="<?php echo $Precio; ?>">
-                        </div>
-                        <div class="form-group">
                             <label>Estado</label>
-                            <input type="text" name="Estado" class="form-control" value="<?php echo $Estado; ?>">
+                            <input type="text" name="Precio" class="form-control" value="<?php echo $Precio; ?>">
                         </div>
                         <div class="form-group">
                             <input type="submit" name="Submit" value="Update" class="btn btn-primary btn-block" name="update">    
