fdb13b5598e51db88e4acc1552a3d63c85ff8a1f
Sebastian <jssanabria918@misena.edu.co>
2021-08-30 22:01:51 -0500

    Testing

--- tests/Operaciones.php
+++ tests/Operaciones.php
@@ -1,11 +1,11 @@
 <?php
       class Operaciones{
-          public function __construct() {
+          public function __constructor() {
             
           }
           public function sum($n1,$n2) {
-            if ($n1 === null || !is_numeric($n1) || !is_numeric($n2)) {
-                throw new InvalidArhumentException("Valores no son numericos");   
+            if ($n1 === null || $n2 ===null || !is_numeric($n1) || !is_numeric($n2)) {
+                throw new InvalidArgumentException("Valores no son numericos");   
             }
             return $n1 +$n2;
           }
@@ -14,7 +14,7 @@
                 throw new InvalidArgumentException("Valores no son numericos");
             }
             if ($n2 == 0) {
-                throw new InvalidArgunebtExeption("Division por cero"); 
+                throw new InvalidArgumentException("Division por cero"); 
             }
             return $n1 / $n2;
           }
