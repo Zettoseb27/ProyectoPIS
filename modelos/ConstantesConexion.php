1ec2e937746be9b830f8574530827f7cbd9d1115
Sebastian <jssanabria918@misena.edu.co>
2021-08-31 11:52:38 -0500

    31 testing

--- modelos/ConstantesConexion.php
+++ modelos/ConstantesConexion.php
@@ -5,7 +5,5 @@ define("BASE","proyecto");//se define una constante para la base, de ahora en ad
 define("SERVIDOR", "localhost");//se define una constante para el servidor, de ahora en adelante SERVIDOR
 define("USUARIO_BD", "root"); //se define una constante para el usuario de la base, de ahora en adelante USUARIO_BD
 define("CONTRASEÑA_BD", "123456"); //se define una constante para la contraseña,de ahora en adelante CONTRASENIA_BD
-
-
-
 define("PATH", $_SERVER['DOCUMENT_ROOT']."/"."ProyectoPIS"."/");
+?>
