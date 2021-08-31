08a7f264cd9cadd9ab03cb26b1f1f1582f3a32a7
Santiago Arevalo Beltran <santiagoarevalobeltran@gmail.com>
2021-08-31 17:27:25 -0500

    TestCocinero

--- tests/TestCocinero/CocineroDAOTest.php
+++ tests/TestCocinero/CocineroDAOTest.php
@@ -27,26 +27,24 @@
                     $this -> conexion= NULL;
                 }
         }
-        class RolDAO extends ConBdMySql {
+        class CocineroDAO extends ConBdMySql {
             public function __construct ($servidor, $base, $loginBD, $passwordBD) {
                 parent::__construct($servidor, $base, $loginBD, $passwordBD);
             }
             public function seleccionarTodos(){
-               $planConsulta = "select rolId, rolNombre, rolDescripcion, rol_created_at
-               from rol;";
-               $registrosOrden = $this -> conexion -> prepare ($planConsulta);
-               $registrosOrden -> execute();//ejecucion de la consulta
-               $listadoRegistroOrden = array();
-               while($registro=$registrosOrden -> fetch(PDO::FETCH_OBJ)){
-                   $listadoRegistroOrden[] = $registro;
+               $planConsulta = "select cocId, cocIdCocinero, cocIdCodigoCocinero, cocCreated_at
+               from cocinero;";
+            
+             
                }
-                   $this->cierreBd();
-                   return $listadoRegistroOrden;
-           }public function seleccionarId($rolId) {
-                $consultar = "select rolId, rolNombre, rolDescripcion, rol_created_at
-                from rol where rolId = ?;";
+                  
+           }
+           
+           public function seleccionarId($cocId) {
+                $consultar = "select cocId, cocIdCocinero, cocIdCodigoCocinero, cocCreated_at
+                from cocinero where cocIdId = ?;";
                 $listar = $this -> conexion -> prepare($consultar);
-                $listar -> execute(array($rolId[0]));
+                $listar -> execute(array($cocId[0]));
                 $registroEncontrado = array();
                 while ($registro = $listar -> fetch(PDO::FETCH_OBJ)) {
                     $registroEncontrado[] = $registro;
@@ -60,15 +58,15 @@
            }
            public function insertar($registro) {
                 try {
-                    $consultar = "insert into rol values (:rolId, :rolNombre, :rolDescripcion, :rolEstado, :rolUsuSesion, :rol_created_at, :rol_updated_at);";
+                    $consultar = "insert into rol values (:cocId, :cocIdCocinero, :cocIdCodigoCocinero, :cocEstado, :cocSesion, :cocCreated_at, :cocUsdated_at);";
                     $insertar = $this -> conexion -> prepare($consultar);
-                    $insertar -> bindParam("rolId", $registro['rolId']);
-                    $insertar -> bindParam("rolNombre", $registro['rolNombre']);
-                    $insertar -> bindParam("rolDescripcion", $registro['rolDescripcion']);
-                    $insertar -> bindParam("rolEstado", $registro['rolEstado']);
-                    $insertar -> bindParam("rolUsuSesion", $registro['rolUsuSesion']);
-                    $insertar -> bindParam("rol_created_at", $registro['rol_created_at']);
-                    $insertar -> bindParam("rol_updated_at", $registro['rol_updated_at']);
+                    $insertar -> bindParam("cocId", $registro['cocId']);
+                    $insertar -> bindParam("cocIdCocinero", $registro['cocIdCocinero']);
+                    $insertar -> bindParam("cocIdCodigoCocinero", $registro['cocIdCodigoCocinero']);
+                    $insertar -> bindParam("cocEstado", $registro['cocEstado']);
+                    $insertar -> bindParam("cocSesion", $registro['cocSesion']);
+                    $insertar -> bindParam("cocCreated_at", $registro['cocCreated_at']);
+                    $insertar -> bindParam("cocUsdated_at", $registro['cocUsdated_at']);
                     $insercion = $insertar ->execute();
                     $clavePrimaria = $this -> conexion -> lastInsertId();
                     return ['inserto' => $insercion, 'resultado' => $clavePrimaria];
@@ -77,37 +75,36 @@
                     return['inserto' => $insercion, $pdoExc -> errorInfo[2]];
                 }
            }
-           public function eliminar($rolId = array()) {
-            $planConsulta = "delete from rol where rolId = :rolId;";
+           public function eliminar($cocId = array()) {
+            $planConsulta = "delete from cocinero where cocId = :cocId;";
             $eliminar = $this -> conexion -> prepare($planConsulta);
-            $eliminar -> bindParam(':rolId', $rolId[0], PDO:: PARAM_INT);    
+            $eliminar -> bindParam(':cocId', $cocId[0], PDO:: PARAM_INT);    
             $resultado = $eliminar->execute();
             $this -> cierreBd();
             if (!empty($resultado)) {
-                return ['eliminar' => TRUE, 'registroEliminado' => array($rolId[0])];
+                return ['eliminar' => TRUE, 'registroEliminado' => array($cocId[0])];
             } else {
-                return ['eliminar' => FALSE, 'registroEliminado' => array($rolId[0])];
+                return ['eliminar' => FALSE, 'registroEliminado' => array($cocId[0])];
             }
            }
             public function actualizar($registro) {
                 try {
-                $nombre = $registro[0]['rolNombre']; 
-                $descripcion = $registro[0]['rolDescripcion'];
-                $cambiarEstado = $registro[0]['rolEstado'];
-                $usuSesion = $registro[0]['rolUsuSesion'];
-                $created = $registro[0]['rol_created_at'];
-                $updated = $registro[0]['rol_updated_at'];
-                $Id = $registro[0]['rolId'];
+                $Id = $registro[0]['cocId']; 
+                $IdCocinero = $registro[0]['cocIdCocinero'];
+                $CodigoCocinero = $registro[0]['cocIdCodigoCocinero'];
+                $Estado = $registro[0]['cocEstado'];
+                $Creacion = $registro[0]['cocCreated_at'];
+                $Usdated = $registro[0]['cocUsdated_at'];
+                $Id = $registro[0]['cocId'];
                 if (isset($Id)) {
-                    $actualizar = "UPDATE rol SET rolNombre=? ,";
-                    $actualizar.= "rolDescripcion=? ,";
-                    $actualizar.= "rolEstado=? ,";
-                    $actualizar.= "rolUsuSesion=? ,";
-                    $actualizar.= "rol_created_at=? ,";
-                    $actualizar.= "rol_updated_at=? ";
-                    $actualizar.= "where rolId=?;";
+                    $actualizar = "UPDATE Cocinero SET cocId=? ,";
+                    $actualizar.= "cocIdCocinero=? ,";
+                    $actualizar.= "cocIdCodigoCocinero=? ,";
+                    $actualizar.= "cocEstado=? ,";
+                    $actualizar.= "cocUsdated_at=? ";
+                    $actualizar.= "where cocId=?;";
                     $actualizacion = $this->conexion->prepare($actualizar);
-                    $resultadoAct=$actualizacion->execute(array($nombre,$descripcion,$cambiarEstado,$usuSesion,$created,$updated, $Id));
+                    $resultadoAct=$actualizacion->execute(array($Id,$IdCocinero,$CodigoCocinero,$Usdated,$Id));
                     $this->cierreBd();
                                 
                     //MEJORAR LA SALIDA DE LOS DATOS DE ACTUALIZACIÓN EXITOSA
@@ -118,26 +115,26 @@
                         return ['actualizacion' => $resultadoAct, 'mensaje' => $pdoExc];
                     }
             }
-            public function habilitar($rolId = array()) {
+            public function habilitar($cocId = array()) {
                 try {
                 $cambiarValorTotal = 1;
-                if (isset($rolId[0])) {
-                    $actualizar = "update rol set rolEstado = ? where rolId = ?;";
+                if (isset($cocId[0])) {
+                    $actualizar = "update Cocinero set cocEstado = ? where cocId = ?;";
                     $actualizacion = $this-> conexion -> prepare($actualizar);
-                    $actualizacion = $actualizacion -> execute(array($cambiarValorTotal, $rolId[0]));
+                    $actualizacion = $actualizacion -> execute(array($cambiarValorTotal, $cocId[0]));
                     return['actualizacion' => $actualizacion, 'mensaje' => "Registro activado"];
                     }
                 }catch (PDOException $pdoExc) {
                     return['actualizacion' => $actualizacion, 'mensaje' => $pdoExc];
                 }
             }
-            public function eliminadorLogico($rolId = array()) {
+            public function eliminadorLogico($cocId = array()) {
                 try {
                 $cambiarEstado = 0;
-                if (isset($rolId[0])) {
-                    $actualizar = "update rol set rolEstado = ? where rolId = ?;";
+                if (isset($cocId[0])) {
+                    $actualizar = "update Cocinero set cocEstado = ? where cocId = ?;";
                     $actualizacion = $this->conexion->prepare($actualizar);
-                    $actualizacion = $actualizacion->execute(array($cambiarEstado, $rolId[0]));
+                    $actualizacion = $actualizacion->execute(array($cambiarEstado, $cocId[0]));
                     return ['actualizacion' => $actualizacion, 'mensaje' => "Registro Desactivado."];
                 }
             } catch (PDOException $pdoExc) {
@@ -148,28 +145,28 @@
 
     }
     use PHPUnit\Framework\TestCase;
-    final class RolDAOTest extends TestCase{
-        private $rol;
+    final class CocineroDAOTest extends TestCase{
+        private $Cocinero;
         public function setUp(): void {
-            $this -> rol = new RolDAO(SERVIDOR, BASE, USUARIO_BD, CONTRASEÑA_BD);
+            $this -> Cocinero = new RolDAO(SERVIDOR, BASE, USUARIO_BD, CONTRASEÑA_BD);
         }
         public function testSeleccionarTodos() {
-            $this ->assertEmpty(!$this -> rol -> seleccionarTodos());
+            $this ->assertEmpty(!$this -> Cocinero -> seleccionarTodos());
         }
         public function testsSeleccionarID($dato = array(1)) {
-            $realizado = $this -> rol -> seleccionarId($dato);
+            $realizado = $this -> Cocinero -> seleccionarId($dato);
             $this->assertTrue(($realizado['exitoSeleccionId']));
         }
-         /*public function testInsertar($dato = array('rolId' => 10, 'rolNombre' => 'Proveedor', 'rolDescripcion'   => 'suministra los protuctos para el restaurante', 'rolEstado' => 1, 'rolUsuSesion'  => null,     'rol_created_at' => '2021-08-12 02:22:00', 'rol_updated_at' =>'2021-08-11 02:22:00')) {
-            $realizado =  $this -> rol -> insertar($dato);
+         /*public function testInsertar($dato = array('rolId' => 10, 'cocId' => 'Id', 'cocIdCocinero'   => 'Generar un codigo al mesero', 'cocEstado' => 1, 'cocCodigoCocinero'  => 550393,     'cocCreated_at' => '2021-08-12 02:22:00', 'cocUpdated_at' =>'2021-08-11 02:22:00')) {
+            $realizado =  $this -> Cocinero -> insertar($dato);
             $this->assertTrue(($realizado["inserto"])); 
         }*/
-        public function testsActualizar($dato =array(array('rolId' => 9, 'rolNombre' => 'Proveedor', 'rolDescripcion'   => 'suministra los protuctos para el restaurante', 'rolEstado' => 1, 'rolUsuSesion'  => null,     'rol_created_at' =>'2021-09-12 03:22:00', 'rol_updated_at' =>'2021-09-10 04:25:00'))) {
-        $realizado = $this->rol->actualizar($dato);
+        public function testsActualizar($dato =array(array('cocId' => 2, 'cocIdCocinero' => 'Generar un codigo al mesero', 'cocEstado'  => '1', 'cocCodigoCocinero' => 550393, 'cocCreated_at'  => '2021-08-12 02:22:00',   'cocUpdated_at' =>'2021-08-11 02:22:00))) {
+        $realizado = $this->Cocinero->actualizar($dato);
         $this->assertTrue(($realizado['actualizacion']));
         }
         public function testEliminar($dato = array(1)) {
-            $realizado = $this->rol->eliminar($dato);
+            $realizado = $this->Cocinero->eliminar($dato);
             $this->assertTrue(($realizado['eliminar']));
         }
 
