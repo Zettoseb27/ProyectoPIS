9143f58ffa02161a16499bf54dcaa172accc5320
Sebastian <jssanabria918@misena.edu.co>
2021-09-06 22:21:49 -0500

    Clase 6 de Septiembre

--- principal.php
+++ principal.php
@@ -1,5 +1,12 @@
 <?php
      session_start();
+
+    include_once 'modelos/ConstantesConexion.php';
+    include_once PATH . 'controladores/ManejoSesiones/BloqueDeSeguridad.php';
+
+    $seguridad= new BloqueDeSeguridad();
+    $seguridad->seguridad("login.php");
+
      if (isset($_SESSION['mensaje'])) {
         $mensaje = $_SESSION['mensaje'];
         echo "<script languaje='javascript'>alert('$mensaje')</script>";
